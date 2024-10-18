2765d60c47eaf2a997c9fabc4a0095d1bcae2fce
fatihua <fatihusameavseren@gmail.com>
2024-10-18 09:56:43 +0200

    nodemodules - dotenv hinzugefügt

--- nodeJS/class-notes/07-NodeModules/index.js
+++ nodeJS/class-notes/07-NodeModules/index.js
@@ -1,4 +1,14 @@
 'use strict'
 /*------------------*/
+
+
+
 console.log("this is indexjs");
+/*--------------------------------------*/
+require("dotenv").config()
+
+
+
+
 console.log(process.env.PORT);
+console.log(process.env.HOST);
